f158df8eccb1cdb7b106ba7168930637ae6be11c
nguyenTanHung11222 <67372290+nguyenTanHung11222@users.noreply.github.com>
2022-10-23 23:32:22 +0700

    sbtemplate

---
+++ src/main/java/com/example/sbtemplate/SbtemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbtemplate/SbtemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
